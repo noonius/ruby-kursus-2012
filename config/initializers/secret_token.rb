d4f00da33f5bc9b329b35193041ca9097e37c5bc
Aret <aret.leetsaar@gmail.com>
2012-02-28 20:12:43 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyKursus2012::Application.config.secret_token = '1fed40fe18d05734e56d550d1393834cbabfca5891df18390190d8bcaa5c842a63457860dfb1c509236e2c21931790a9ed7104c871b45985081723acb3fccd4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RubyKursus2012::Application.config.session_store :cookie_store, key: '_ruby-kursus-2012_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RubyKursus2012::Application.config.session_store :active_record_store
