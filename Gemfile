84042f58de4a6ac9a6b375cec748ac71872bca98
tanel <tanel@noonius.net>
2012-02-29 10:52:57 +0200

    Static pages from old page

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.1'
 
 gem 'sqlite3'
 
+gem 'coderay'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,8 @@ RubyKursus2012::Application.routes.draw do
 
   resources :projects
   match '/links' => 'static#links'
+  match '/lisamaterjalid' => 'static#links'
+  match '/projektid' => 'projects#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
