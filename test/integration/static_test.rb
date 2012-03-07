1ab1cd876d86c735b87bae040abf88d0f005299a
Aret <aret.leetsaar@gmail.com>
2012-03-07 13:24:10 +0200

    project mod

--- test/integration/static_test.rb
+++ test/integration/static_test.rb
@@ -2,14 +2,9 @@ require "minitest_helper"
 
 describe "Static Integration Test" do
   context "static#index" do
-    before do
-      get "/"
-    end
-    it { must_respond_with :success }
-    #capybara usage example
     it "website index page must be accessible" do
-      visit static_index_path
-      page.must_have_content("Static#index")
+      visit root_path
+      page.has_content?("Static#index").must_be true
     end
   end
 end
