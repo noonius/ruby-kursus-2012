7499a5c5ed27d8d28fbddad5a871dd575a826117
tanel <tanel@noonius.net>
2012-03-14 11:37:53 +0200

    Modify projects view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,30 +1,30 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @projects }
-    end
   end
   
-  def edit
-    @project = Project.find(params[:id])
-  end
-
-  def update
-    @project = Project.find(params[:id])
-
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to(@project, :notice => 'Product was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+  # def edit
+  #     if Rails.env == 'production'
+  #       redirect_to :action => :index and return
+  #     end
+  #     @project = Project.find(params[:id])
+  #   end
+  # 
+  #   def update
+  #     if Rails.env == 'production'
+  #       redirect_to :action => :index and return
+  #     end
+  #     @project = Project.find(params[:id])
+  #     respond_to do |format|
+  #       if @project.update_attributes(params[:project])
+  #         format.html { redirect_to(@project, :notice => 'Product was successfully updated.') }
+  #         format.xml  { head :ok }
+  #       else
+  #         format.html { render :action => "edit" }
+  #         format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
+  #       end
+  #     end
+  #   end
   
   def show
     redirect_to :action => :index
