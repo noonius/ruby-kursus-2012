2d1d974be4dc0cfaeb3fab56eac0124b00b50038
Aret <aret.leetsaar@gmail.com>
2012-03-21 11:33:10 +0200

    no editing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,28 +10,28 @@ class ProjectsController < ApplicationController
     end
   end
   
-  def edit
-   if Rails.env == 'production'
-     redirect_to :action => :index and return
-   end
-   @project = Project.find(params[:id])
-  end
-
-  def update
-   if Rails.env == 'production'
-     redirect_to :action => :index and return
-   end
-   @project = Project.find(params[:id])
-   respond_to do |format|
-     if @project.update_attributes(params[:project])
-       format.html { redirect_to(@project, :notice => 'Product was successfully updated.') }
-       format.xml  { head :ok }
-     else
-       format.html { render :action => "edit" }
-       format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
-     end
-   end
-  end
+#  def edit
+#   if Rails.env == 'production'
+#     redirect_to :action => :index and return
+#   end
+#   @project = Project.find(params[:id])
+#  end
+#
+#  def update
+#   if Rails.env == 'production'
+#     redirect_to :action => :index and return
+#   end
+#   @project = Project.find(params[:id])
+#   respond_to do |format|
+#     if @project.update_attributes(params[:project])
+#       format.html { redirect_to(@project, :notice => 'Product was successfully updated.') }
+#       format.xml  { head :ok }
+#     else
+#       format.html { render :action => "edit" }
+#       format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
+#     end
+#   end
+#  end
 
   def show
     redirect_to :action => :index
